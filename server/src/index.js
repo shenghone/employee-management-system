99cabb0d512da30eac363b645aa40143da790d5d
shenghone <shenghone@gmail.com>
2022-04-08 06:29:01 -0600

    fixing cors

--- server/src/index.js
+++ server/src/index.js
@@ -59,6 +59,15 @@ require("dotenv").config();
       //methods: ['GET', 'PUT', 'POST', 'OPTIONS'],
     };
     app.use(cors(corsOptions));
+    app.use(function(req, res, next) {
+      res.header('Content-Type', 'application/json;charset=UTF-8')
+      res.header('Access-Control-Allow-Credentials', true)
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+      )
+      next()
+    })
     const server = new ApolloServer({
       typeDefs,
       resolvers,
